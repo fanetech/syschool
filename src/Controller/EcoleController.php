9d676658d8cdaea48fd69256d3415be4e9fdfac0
fanetech <louguefranck.20@gmail.com>
2022-05-12 12:41:00 +0000

    generation des template de base et gestion du login

---
+++ src/Controller/EcoleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EcoleController extends AbstractController
+{
+    #[Route('/ecole', name: 'ecole.dashboard')]
+    public function index(): Response
+    {
+        return $this->render('ecole/index.html.twig', [
+            'controller_name' => 'EcoleController',
+        ]);
+    }
+}
